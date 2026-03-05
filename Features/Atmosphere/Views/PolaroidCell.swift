53b36fd155c247b68ce01e753ad6929e76e597e2
ilaydacelikkayaa <ilaydacelikkaya2@gmail.com>
2026-03-05 14:32:01 +0300

    feat: add polaroid section to the main view

---
+++ Features/Atmosphere/Views/PolaroidCell.swift
@@ -0,0 +1,8 @@
+//
+//  PolaroidCell.swift
+//  Atmosphere
+//
+//  Created by İlayda Çelikkaya on 2.03.2026.
+//
+
+import Foundation
---
+++ Features/Atmosphere/Views/PolaroidView.swift
@@ -0,0 +1,8 @@
+//
+//  PolaroidView.swift
+//  Atmosphere
+//
+//  Created by İlayda Çelikkaya on 2.03.2026.
+//
+
+import Foundation
